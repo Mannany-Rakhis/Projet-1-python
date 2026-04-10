277af3bf4490a2678182a2d79624370e22366668
Noémie Niamien <noemienmn@gmail.com>
2026-04-10 11:52:12 +0200

    feat:I implemented a feature that allows users to select the number of players in a game session

---
+++ pfc.py
@@ -0,0 +1,12 @@
+def nombres_joueurs():
+    joueurs=int(input("Entrer le nombre de joueurs:"))
+    if joueurs==16:
+        print("Veuillez entrer les noms des participants:")
+    elif joueurs==8:
+        print( "Veuillez entrer les noms des participants:")
+    elif joueurs==4:
+        print("Veuillez entrer les noms des participants:")
+    else:
+        print("Entrer un nombre valide")
+nombres_joueurs()
+        
