16e7f247dfb0e9f54faec6d9e3efeff202414da8
Izayoii <rakhisahmat3600@gmail.com>
2026-04-10 11:36:08 +0200

    feat: return number of players from menu

--- interface-display.py
+++ interface-display.py
@@ -1,18 +1,24 @@
-''' Fonction : menu
-role : afficher les options du tournoi
-et récupérer le choix de l'utilisateur '''
+# Menu du tournoi
 def menu():
     print("=== TOURNOI ===")
     print("1. 4 joueurs")
     print("2. 8 joueurs")
     print("3. 16 joueurs")
 
-    # Demande à l'utilisateur de choisir une option
-    choix = input("Choix : ")
+    choix = input("Choix : ")  # récupération du choix
 
-    # Affiche le choix pour vérification
-    print("Tu as choisi l'option :", choix)
+    # on retourne le nombre de joueurs correspondant
+    if choix == "1":
+        return 4
+    elif choix == "2":
+        return 8
+    elif choix == "3":
+        return 16
+    else:
+        return 4  # valeur par défaut si erreur
 
 
-# Appel de la fonction pour lancer le menu
-menu()
+# appel du menu
+nb_joueurs = menu()
+
+print("Nombre de joueurs :", nb_joueurs)
